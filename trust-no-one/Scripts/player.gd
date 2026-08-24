9d33460b1bb62c8cb134412166bc1e01b0e077e5
Your Name <your.email@example.com>
2026-08-24 19:09:34 +0100

    basic platform and spike scene

--- trust-no-one/Scripts/player.gd
+++ trust-no-one/Scripts/player.gd
@@ -1,28 +1,23 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if Input.is_action_just_pressed("menu"):
-		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-		
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# I added to the standard ui_directions arrow key controls, A,D movement.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if Input.is_action_just_pressed("menu"):
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 	move_and_slide()
